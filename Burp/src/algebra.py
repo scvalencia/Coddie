992357b214ce75ca53fe81207c117f17b66eaa56
Sebastián Valencia <sc.valencia606@gmail.com>
2015-01-15 11:32:41 -0500

    More API operations

--- Burp/src/algebra.py
+++ Burp/src/algebra.py
@@ -1,5 +1,7 @@
 import relation
 
+# Set operators
+
 def union(r1, r2):
 	return r1.union(r2)
 
@@ -11,3 +13,11 @@ def difference(r1, r2):
 
 def cross(r1, r2):
 	return r1.cross(r2)
+
+# Relational operators
+
+def project(r1, attributes):
+	return r1.project(attributes)
+
+def select(r1, foo):
+	return r1.select(foo)
