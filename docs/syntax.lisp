4047c1701ef6e25ae136a7507aa2b6d892088b34
scvalencia <sc.valencia606@uniandes.edu.co>
2016-07-05 19:26:46 -0500

    Adding features to burp in the I/O part of it

--- docs/syntax.lisp
+++ docs/syntax.lisp
@@ -35,5 +35,32 @@
 (display (project student (lastname program)))
 (display student)
 
+;; saves a single relation, given by an expression that evaluates to a relation
+;; to a file named as filename given as a string "model.burp", if possible,
+;; otherwise, it would report an appropiate error message
+
+(save relation filename)
+
+;; saves every relation in the enviroment to a file named as filename 
+;; given as a string "model.burp", if possible, otherwise, it would 
+;; report an appropiate error message
+
+(save * filename)
+
+;; saves every specified relation, given by several expressions that 
+;; evaluates to relations to a file named as filename given as a string 
+;; "model.burp", if possible, otherwise, it would report an appropiate 
+;; error message
+
+(save (relation1 relation2 ... relationn) filename)
+
+;; exports the given relation using the specified option,
+;; if option is "latex", it prints the LaTeX representation of the relation
+;; if caption is given, it would be the caption of the LaTeX table. caption,
+;; should be an string as in "This is a valid caption"
+;; relation is an expression that evaluates to an expression
+
+(export relation option caption?)
+
 
 
