0a411f8e7765dc5108f59a937ca880fa04edd2ba
scvalencia <sc.valencia606@uniandes.edu.co>
2016-07-08 17:07:11 -0500

    Kernel support for new algebraic operators, those based on sets

--- src/relation.py
+++ src/relation.py
@@ -54,6 +54,9 @@ class Relation(object):
 	_ERROR04 = ('Error while executing projection operator on %s. %s is not'
 					'in the relation\'s schema')
 
+	_ERROR05 = ('Error while executing set operator: %s. Relation %s, and '
+					'relation %s, are not type-compatible')
+
 	def __init__(self, name, types, attributes):
 		if len(types) != len(attributes):
 			_callerror(self._ERROR01 % (name, len(types), len(attributes)))
@@ -109,6 +112,60 @@ class Relation(object):
 
 		return resulting
 
+	def type_compatible(self, that):
+		return self.types == that.types
+
+	def union(self, that):
+		if not self.type_compatible(that):
+			_callerror(self._ERROR05 % \
+				('union', self.name, that.name))
+			return
+
+		resulting_name = self.name + '_union_' + \
+			that.name + '_' + self._random_string()
+		resulting_type = self.types
+		resulting_tuples = dict(list(self.tuples.items()) + list(that.tuples.items()))
+		resulting_relation = Relation(resulting_name, resulting_type, self.attributes)
+
+		for tpl in resulting_tuples: 
+			resulting_relation.insert(resulting_tuples[tpl].data)
+
+		return resulting_relation
+
+	def intersection(self, that):
+		if not self.type_compatible(that):
+			_callerror(self._ERROR05 % \
+				('inter', self.name, that.name))
+			return
+
+		resulting_name = self.name + '_inter_' + \
+			that.name + '_' + self._random_string()
+		resulting_type = self.types
+		resulting_tuples = {x : self.tuples[x] for x in self.tuples if x in that.tuples}
+		resulting_relation = Relation(resulting_name, resulting_type, self.attributes)
+
+		for tpl in resulting_tuples: 
+			resulting_relation.insert(resulting_tuples[tpl].data)
+
+		return resulting_relation
+
+	def difference(self, that):
+		if not self.type_compatible(that):
+			_callerror(self._ERROR05 % \
+				('diff', self.name, that.name))
+			return
+
+		resulting_name = self.name + '_diff_' + \
+			that.name + '_' + self._random_string()
+		resulting_type = self.types
+		resulting_tuples = {x : self.tuples[x] for x in self.tuples if x not in that.tuples}
+		resulting_relation = Relation(resulting_name, resulting_type, self.attributes)
+
+		for tpl in resulting_tuples: 
+			resulting_relation.insert(resulting_tuples[tpl].data)
+
+		return resulting_relation
+
 	def display(self):
 		table = []
 
@@ -128,8 +185,9 @@ class Relation(object):
 		for tpl in self.tuples:
 			aux = 'INSERT ('
 			value = self.tuples[tpl].data
-			for itm in value:
-				aux += itm + ', '
+
+			for itm in value: aux += itm + ', '
+
 			aux = aux[:-2] + ') INTO %s\n' % self.name
 			ans += aux
 
@@ -143,8 +201,7 @@ class Relation(object):
 	def tolatex(self, caption=''):
 		latex, header = '\\begin{table}[h!]\n\centering\n', ''
 
-		for itm in self.attributes: 
-			header += itm + ' & '
+		for itm in self.attributes:  header += itm + ' & '
 
 		header = header[:-3]
 		latex += '\\begin{tabular}{||%s||}\n' % (self.arity * 'c ')[:-1]
@@ -166,3 +223,54 @@ class Relation(object):
 		latex += '\end{table}\n'
 
 		return latex
+
+'''
+r1 = Relation('employee', ['STRING', 'STRING', 'INTEGER'], ['name', 'lastname', 'salary'])
+r2 = Relation('person', ['STRING', 'STRING', 'INTEGER'], ['name', 'lastname', 'age'])
+r1.insert(('"Alonso"', '"Perez"', '100'))
+r1.insert(('"Rodolfo"', '"Benitez"', '50'))
+r2.insert(('"Elizabeth"', '"Baranza"', '45'))
+r2.insert(('"Rodolfo"', '"Benitez"', '50'))
+
+t1 = Tuple(("Roberto", "Almagro"))
+t2 = Tuple(("Almagro", "Roberto"))
+t3 = Tuple(("Almagro", "Roberto"))
+
+
+print t1.hash
+print t2.hash
+print t3.hash
+# r.display()
+# r.delete(('"Rodolfo"', '"Benitez"', '50'))
+r1.display()
+r2.display()
+
+r3 = r1.union(r2)
+
+r3.display()
+
+r4 = r1.intersection(r2)
+r4.display()
+
+r5 = r2.difference(r1)
+r5.display()
+
+graduates = Relation('graduates', ['INTEGER', 'STRING', 'INTEGER'], ['number', 'surname', 'age'])
+graduates.insert(('7274', '"Robinson"', '37'))
+graduates.insert(('7432', '"O\'Malley"', '39'))
+graduates.insert(('9824', '"Darkes"', '38'))
+
+managers = Relation('managers', ['INTEGER', 'STRING', 'INTEGER'], ['number', 'surname', 'age'])
+managers.insert(('9297', '"O\'Malley"', '56'))
+managers.insert(('7432', '"O\'Malley"', '39'))
+managers.insert(('9824', '"Darkes"', '38'))
+
+union = graduates.union(managers)
+union.display()
+
+inter = graduates.intersection(managers)
+inter.display()
+
+diff = graduates.difference(managers)
+diff.display()
+'''
